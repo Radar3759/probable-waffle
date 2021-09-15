4e6c443b5820d7d4067c746e069d3319331c6310
Radar3759 <bunnybridgemitten@gmail.com>
2021-09-15 13:40:21 -0700

    Update tip-calculator.py
    add more complex tip-calculator

--- tip-calculator.py
+++ tip-calculator.py
@@ -6,7 +6,7 @@
 #HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
 
 
-## two tests simple and with inputs
+## two tests
 
 #simple test
 bill = 150
@@ -18,4 +18,25 @@ each_pays = (bill + tip) / num_people
 #format '.2f' makes 2 decimal places
 print(format(each_pays,'.2f'))
 
-#add inputs here
+#more complex
+
+print("Welcome to the tip calculator")
+
+bill = float(input("What was the total bill "))
+tip_percent = int(input("What percent would you like to tip? 10, 12, or 15 "))
+num_people = int(input("How many people split the bill? "))
+
+#turns the tip percent into a decimal
+tip_num = (tip_percent) / 100
+#total plus tip is 1.xx * bill
+bill_total = (1 + tip_num) * bill
+print(bill_total)
+
+#how much does each person play
+each_pays = bill_total / num_people
+#format '.2f' makes 2 decimal places
+final_amount = "{:.2f}".format(each_pays)
+
+print(f"Each person pays: ${final_amount}")
+
+
